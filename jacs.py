3209753e4cec9871338c2d5c9d0862a10ef54ca9
RMgX-161 <68577500+RMgX-161@users.noreply.github.com>
2021-02-18 16:42:50 +0800

    Delete jacs.py

--- jacs.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-
-vol=125
-page=6042
-browser=webdriver.Chrome()
-browser.get('https://pubs.acs.org/action/quickLink?quickLink=true&quickLinkJournal=jacsat&quickLinkVolume={}&quickLinkPage={}'.format(str(vol),str(page)))
